d648d62df6def3963b587d92bc9674be1315493b
Amit Wani <mtwn105@gmail.com>
2023-06-03 17:43:24 +0530

    Appwrite installed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Api } from './helpers/api';
+import { Account, ID } from 'appwrite';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'donor-match';
+
+  ngOnInit(): void {
+    // const account = new Account(Api.provider());
+
+    // // Register User
+    // account.create(
+    //   ID.unique(),
+    //   'me@example.com',
+    //   'password',
+    //   'Jane Doe'
+    // ).then(response => {
+    //   console.log(response);
+    // }, error => {
+    //   console.log(error);
+    // });
+  }
+
 }
